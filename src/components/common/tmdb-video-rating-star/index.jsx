028026221f152628df7f20c33b7e247c5500889e
yizzycool <chsh110768@gmail.com>
2024-11-13 02:07:42 +0800

    [what] update TmdbVideoRatingStar: add an option: stringInNewLine.

--- src/components/common/tmdb-video-rating-star/index.jsx
+++ src/components/common/tmdb-video-rating-star/index.jsx
@@ -8,6 +8,7 @@ export default function TmdbVideoRatingStar({
   base = 10,
   voteCount = 0,
   showString = true,
+  stringInNewLine = true,
 }) {
   // Transform to format of 'x / 5'
   const transRating = _round((rating * BASE) / base, 1);
@@ -22,23 +23,23 @@ export default function TmdbVideoRatingStar({
   return (
     <div>
       {_range(fullStar).map((idx) => (
-        <i key={idx} class="bi bi-star-fill text-warning me-1" />
+        <i key={idx} className="bi bi-star-fill text-warning me-1" />
       ))}
       {_range(halfStar).map((idx) => (
-        <i key={idx} class="bi bi-star-half text-warning me-1" />
+        <i key={idx} className="bi bi-star-half text-warning me-1" />
       ))}
       {_range(emptyStar).map((idx) => (
-        <i key={idx} class="bi bi-star me-1" />
+        <i key={idx} className="bi bi-star me-1" />
       ))}
       {showString && (
-        <span>
+        <div className={!stringInNewLine ? 'd-inline' : ''}>
           {_round(rating, 1)}
           {'/'}
           {base}{' '}
           {voteCount > 0 && (
             <span className="text-body-tertiary">({voteCount})</span>
           )}
-        </span>
+        </div>
       )}
     </div>
   );
