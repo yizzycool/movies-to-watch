fc5c49e587d58e5f369f178e3923ab746042c309
yizzycool <chsh110768@gmail.com>
2024-11-17 14:49:25 +0800

    [what] split infinite scroll into a new component to be reused further.

---
+++ src/components/common/infinite-scroll-movie-list/index.jsx
@@ -0,0 +1,75 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import useIntersectionObserver from '@/hooks/use-intersection-observer';
+import TmdbImage, { TmdbImageTypes } from '@/components/common/tmdb-image';
+import TmdbMovieHoverMask from '@/components/common/tmdb-movie-hover-mask';
+import LoadingSkeleton from './loading-skeleton';
+import _get from 'lodash/get';
+import _isEmpty from 'lodash/isEmpty';
+
+export default function InfiniteScrollMovieList({
+  fetchedData,
+  isFetching,
+  onNext = () => {},
+}) {
+  const router = useRouter();
+
+  // Use this hook to trigger infinite scroll
+  const { hit } = useIntersectionObserver({
+    startDetect: !_isEmpty(fetchedData),
+    targetSelector: '#result-spinner',
+    threshold: 0.5,
+  });
+
+  // Call API to query next-page data when scroll to bottom
+  useEffect(() => {
+    if (!hit) return;
+    onNext();
+  }, [hit]);
+
+  const getMovieId = (result) => _get(result, 'id', null);
+
+  const getPosterPath = (result) => _get(result, 'poster_path', '');
+
+  // Navigate to /movie page
+  const onMovieClick = (result) => {
+    const url = `/movie?id=${getMovieId(result)}`;
+    router.push(url);
+  };
+
+  return (
+    <>
+      {_isEmpty(fetchedData) ? (
+        <LoadingSkeleton />
+      ) : (
+        <div className="row gx-3 gy-3 my-5">
+          {fetchedData?.results.map((result, idx) => (
+            <div key={idx} className="col-6 col-sm-4 col-md-3 col-lg-2">
+              <div
+                className="position-relative ratio rounded overflow-hidden"
+                style={{ '--bs-aspect-ratio': '150%' }}
+              >
+                <TmdbImage
+                  linkTo={`/movie?id=${getMovieId(result)}`}
+                  path={getPosterPath(result)}
+                  type={TmdbImageTypes.poster}
+                />
+                <TmdbMovieHoverMask
+                  result={result}
+                  onClick={() => onMovieClick(result)}
+                />
+              </div>
+              <div className="fw-bold mt-2">
+                {_get(result, 'original_title')}
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+      <div
+        id="result-spinner"
+        className={`spinner-border mb-5 ${!isFetching ? 'opacity-0' : ''}`}
+      />
+    </>
+  );
+}
--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useLazySearchMoviesQuery } from '@/store/apis/tmdb';
-import useIntersectionObserver from '@/hooks/use-intersection-observer';
-import TmdbImage, { TmdbImageTypes } from '@/components/common/tmdb-image';
-import TmdbMovieHoverMask from '../common/tmdb-movie-hover-mask';
-import LoadingSkeleton from './loading-skeleton';
+import InfiniteScrollMovieList from '../common/infinite-scroll-movie-list';
 import _get from 'lodash/get';
 import _isEmpty from 'lodash/isEmpty';
 
@@ -18,13 +15,6 @@ export default function Search() {
   const [trigger, result, lastPromiseInfo] = useLazySearchMoviesQuery();
   const { data, isFetching } = result;
 
-  // Use this hook to trigger infinite scroll
-  const { hit } = useIntersectionObserver({
-    startDetect: !_isEmpty(fetchedData),
-    targetSelector: '#search-result-spinner',
-    threshold: 0.5,
-  });
-
   // Call API when 'query' detected
   useEffect(() => {
     if (!query) return;
@@ -34,16 +24,7 @@ export default function Search() {
     trigger({ query });
   }, [trigger, query]);
 
-  // Call API again when scroll to bottom
-  useEffect(() => {
-    if (!hit || _isEmpty(data)) return;
-    const page = _get(data, 'page', 0);
-    const totalPages = _get(data, 'total_pages', 0);
-    if (page >= totalPages) return;
-    trigger({ query, page: page + 1 });
-  }, [hit]);
-
-  // update data
+  // Update data
   useEffect(() => {
     if (_isEmpty(data)) return;
     const { page, total_pages, total_results, results } = data;
@@ -55,53 +36,24 @@ export default function Search() {
     }));
   }, [data]);
 
-  const getMovieId = (result) => _get(result, 'id', null);
-
-  const getPosterPath = (result) => _get(result, 'poster_path', '');
-
-  // Navigate to /movie page
-  const onMovieClick = (result) => {
-    const url = `/movie?id=${getMovieId(result)}`;
-    router.push(url);
+  // Call API again when scroll to bottom
+  const onNext = () => {
+    if (isFetching) return;
+    const page = _get(data, 'page', 0);
+    const totalPages = _get(data, 'total_pages', 0);
+    if (page >= totalPages) return;
+    trigger({ query, page: page + 1 });
   };
 
   return (
     <div className="container-xl text-center">
-      {_isEmpty(fetchedData) ? (
-        <LoadingSkeleton />
-      ) : (
-        <>
-          <div className="fs-3 mt-5">
-            Search Results for <span className="text-info">{query}</span>
-          </div>
-          <div className="row gx-3 gy-3 my-5">
-            {fetchedData?.results.map((result, idx) => (
-              <div key={idx} className="col-6 col-sm-4 col-md-3 col-lg-2">
-                <div
-                  className="position-relative ratio rounded overflow-hidden"
-                  style={{ '--bs-aspect-ratio': '150%' }}
-                >
-                  <TmdbImage
-                    linkTo={`/movie?id=${getMovieId(result)}`}
-                    path={getPosterPath(result)}
-                    type={TmdbImageTypes.poster}
-                  />
-                  <TmdbMovieHoverMask
-                    result={result}
-                    onClick={() => onMovieClick(result)}
-                  />
-                </div>
-                <div className="fw-bold mt-2">
-                  {_get(result, 'original_title')}
-                </div>
-              </div>
-            ))}
-          </div>
-        </>
-      )}
-      <div
-        id="search-result-spinner"
-        className={`spinner-border mb-5 ${!isFetching ? 'opacity-0' : ''}`}
+      <div className="fs-3 mt-5">
+        Search Results for <span className="text-info">{query}</span>
+      </div>
+      <InfiniteScrollMovieList
+        fetchedData={fetchedData}
+        isFetching={isFetching}
+        onNext={onNext}
       />
     </div>
   );
