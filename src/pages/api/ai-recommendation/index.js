5be491ed5a5e6eaa665b77183bf64e833ec5b7c3
yizzycool <chsh110768@gmail.com>
2024-11-19 22:28:57 +0800

    [what] fix: try-catch to handle error in /api.

--- src/pages/api/ai-recommendation/index.js
+++ src/pages/api/ai-recommendation/index.js
@@ -48,7 +48,7 @@ export default async function handler(req, res) {
       const aiRecommendedData = await inference(body);
       res.status(200).json({ recommendations: aiRecommendedData });
     } catch (e) {
-      res.status(500).json({ error: 'Internal Server Error' });
+      res.status(500).json({ error: e.message });
     }
   } else {
     res.status(405).send({ error: 'Method Not Allowed' });
--- src/pages/api/tmdb/[...slug].js
+++ src/pages/api/tmdb/[...slug].js
@@ -13,23 +13,26 @@ export const config = {
 // other request methods will get error 405 response!
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    const { slug } = req.query;
-    const externalPath = slug.join('/');
-    // Get searchParams from 'req'
-    const searchParams = new URLSearchParams(req.query).toString();
-    // Construct url to be sent
-    const url = `${process.env.TMDB_API_BASE_URL}/${externalPath}${searchParams ? `?${searchParams}` : ''}`;
-    const response = await fetch(url, {
-      method: req.method,
-      headers: {
-        'Content-Type': 'application/json',
-        // Add access token
-        Authorization: `Bearer ${process.env.TMDB_API_ACCESS_TOKEN}`,
-        ...req.headers,
-      },
-    });
-    const data = await response.json();
-    res.status(response.status).json(data);
+    try {
+      const { slug, ...urlParams } = req.query;
+      const externalPath = slug.join('/');
+      // Get searchParams from 'req'
+      const searchParams = new URLSearchParams(urlParams).toString();
+      // Construct url to be sent
+      const url = `${process.env.TMDB_API_BASE_URL}${externalPath}${searchParams ? `?${searchParams}` : ''}`;
+      const response = await fetch(url, {
+        method: req.method,
+        headers: {
+          // Add access token
+          Authorization: `Bearer ${process.env.TMDB_API_ACCESS_TOKEN}`,
+          accept: 'application/json',
+        },
+      });
+      const data = await response.json();
+      res.status(response.status).json(data);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   } else {
     res.status(405).send({ error: 'Method Not Allowed' });
   }
